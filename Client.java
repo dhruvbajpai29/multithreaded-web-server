d7a758a46ee68e5602d9a0cab395c7932cef4c36
Dhruv Bajpai <dhruvbajpai103@gmail.com>
2025-10-10 19:19:51 +0530

    Enhance Client to Simulate Concurrent Load

--- Client.java
+++ Client.java
@@ -1,52 +1,37 @@
 
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
-public class Client {
+public class Client implements Runnable {
 
-    public Runnable getRunnable() throws UnknownHostException, IOException {
-        return new Runnable() {
-            @Override
-            public void run() {
-                int port = 8010;
-                try {
-                    InetAddress address = InetAddress.getByName("localhost");
-                    Socket socket = new Socket(address, port);
-                    try (
-                            PrintWriter toSocket = new PrintWriter(socket.getOutputStream(), true);
-                            BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()))
-                    ) {
-                        toSocket.println("Hello from Client " + socket.getLocalSocketAddress());
-                        String line = fromSocket.readLine();
-                        System.out.println("Response from Server " + line);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    // The socket will be closed automatically when leaving the try-with-resources block
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+    @Override
+    public void run() {
+        String hostname = "localhost";
+        int port = 8010;
 
-            }
-        };
+        try (Socket socket = new Socket(hostname, port)) {
+            PrintWriter toSocket = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            toSocket.println("Hello from Client " + Thread.currentThread().getName());
+            String line = fromSocket.readLine();
+            System.out.println(Thread.currentThread().getName() + " received: " + line);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void main(String[] args){
-        Client client = new Client();
-        for(int i=0; i<100; i++){
-            try{
-                Thread thread = new Thread(client.getRunnable());
-                thread.start();
-            }catch(Exception ex){
-                return;
-            }
+    public static void main(String[] args) {
+        System.out.println("Starting 10 client threads...");
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(new Client());
+            thread.start();
         }
-        return;
     }
 }
